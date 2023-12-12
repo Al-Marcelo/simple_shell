62b128bec3a7c0224c0d6cb50637e67f9e31195b
Marcel Onyeukwu <onyeukwum4@gmail.com>
2023-12-12 15:54:06 -0800

    shell.h file with prototype

---
+++ shell.h
@@ -0,0 +1,27 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/**
+ * main - shell header file
+ *
+ * return: Always 0
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+/**
+ * main - shell project header
+ *
+ * return: Always 0
+ */
+
+void blink_prompt(void);
+void marc_print(const char string);
+void read_command(char *command, size_t size);
+void execute_command(const char *command);
+
+#endif

607dd9b69eda01187ac26ed68e4a9a23bf1c887c
Marcel Onyeukwu <onyeukwum4@gmail.com>
2023-12-14 06:57:52 -0800

    simple betty task

---
+++ bettytask.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * add - add two variables
+ * @num1: the number to be added
+ * @num2: the second number to be added
+ * Return: the added value
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}

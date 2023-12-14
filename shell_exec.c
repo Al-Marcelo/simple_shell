d7c05874a41ef5017ac8d71486880d1d6722e36b
Marcel Onyeukwu <onyeukwum4@gmail.com>
2023-12-14 07:19:40 -0800

    execlp removed and replaced with execve

--- shell_exec.c
+++ shell_exec.c
@@ -1,17 +1,27 @@
 #include "shell.h"
 
-void execute_command(const char *command) {
+/**
+ * execute_command - execute the command
+ * @command: Command to be executed
+ */
+
+void execute_command(const char *command)
+{
 	pid_t child_pid = fork();
 
-	if (child_pid == -1) {
+	if (child_pid == -1)
+	{
 		perror("fork");
 		exit(EXIT_FAILURE);
-	} else if (child_pid == 0) {
+	}
+	else if (child_pid == 0)
+	{
 
-		execlp(command, command, (char *)NULL);
-		perror("execlp");
+		execve(command, command, (char *)NULL);
+		perror("execve");
 		exit(EXIT_FAILURE);
-	} else {
+	} else
+	{
 		wait(NULL);
 	}
 }
